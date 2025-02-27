ae4270f71099e9f81f98b6f18776103b52df61d0
voalvu <48533747+voalvu@users.noreply.github.com>
2025-02-27 13:00:24 +0100

    Create spanishdict.js
    making it easier for me to get and see exactly what i'm looking for with the least amount of actions possible, for convenience

---
+++ spanishdict.js
@@ -0,0 +1,46 @@
+// ==UserScript==
+// @name         Auto-Click Button on SpanishDict (SPA Support) and reformatting page layout
+// @namespace    http://tampermonkey.net/
+// @version      1.1
+// @description  Automatically clicks a specific button on SpanishDict, even on URL changes in the single-page app.
+// @author       Your Name
+// @match        https://www.spanishdict.com/*
+// @grant        none
+// ==/UserScript==
+(function() {
+    'use strict';
+
+    const BUTTON_XPATH = '/html/body/div/div/div[1]/div[2]/div[1]/div[4]/div/ul/li[2]/div/a';
+    const SELECTORS = {
+        remove: ['.OwauVllX', '.TZgqctN1'],
+        flex: '._RHBaSfJ',
+        width: '.WuDqSfpG',
+        participles: { section: '#sd-participles-section', container: '.uVbNQIMZ' }
+    };
+
+    const clickButton = () => {
+        const button = document.evaluate(BUTTON_XPATH, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+        if (button) button.click();
+    };
+
+    const cleanAndFormatPage = () => {
+        SELECTORS.remove.forEach(selector => document.querySelector(selector)?.remove());
+        document.querySelector(SELECTORS.flex)?.style.setProperty('display', 'flex');
+        document.querySelector(SELECTORS.width)?.style.setProperty('width', 'auto');
+
+        const participlesSection = document.querySelector(SELECTORS.participles.section);
+        const participlesContainer = document.querySelector(SELECTORS.participles.container);
+        if (participlesSection && participlesContainer && !participlesContainer.contains(participlesSection)) {
+            participlesContainer.appendChild(participlesSection);
+            participlesContainer.style.gridTemplateColumns = '1fr auto auto';
+        }
+    };
+
+    const observer = new MutationObserver(() => {
+        cleanAndFormatPage();
+        clickButton();
+    });
+
+    observer.observe(document.body, { childList: true, subtree: true });
+    clickButton();
+})();
